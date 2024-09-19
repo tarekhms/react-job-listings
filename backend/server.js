cf1fc85bafe84f7d1f357911bade722eb97a21a0
Tarek <tarek.alhomsi@hotmail.com>
2024-09-20 01:42:31 +0200

    add backend api

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
+connectDB();
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/users/', userRoutes);
+app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Started on port ${port}`));
